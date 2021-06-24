8cd9e7d4da790b760939b2a7609a0d9320c9b755
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-06-24 23:26:13 +0900

    「issue6」
    「issue6」

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -5,7 +5,6 @@ class CalendarsController < ApplicationController
     getWeek
     @plan = Plan.new
   end
-
   # 予定の保存
   def create
     binding.pry
@@ -36,9 +35,16 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+
+      wday_num = Date.today.wday
+    # wdayメソッドを用いて取得した数値
+      if 
+        wday_num = wday_num -7+x
+      end
+
+
+      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans,:wday => wdays[wday_num]}
       @week_days.push(days)
     end
-
   end
 end
