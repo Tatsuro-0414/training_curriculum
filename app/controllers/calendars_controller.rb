01c56e093c8ab1a6066eb3fb06a6021a04e148f6
Tatsuro-0414 <83904503+Tatsuro-0414@users.noreply.github.com>
2021-06-25 21:10:14 +0900

    「issue6」0625
    「issue6」0625

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -36,10 +36,10 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wday_num = Date.today.wday
+      wday_num = Date.today.wday +x
     # wdayメソッドを用いて取得した数値
-      if 
-        wday_num = wday_num -7+x
+      if wday_num >= 7
+        wday_num = wday_num -7
       end
 
 
